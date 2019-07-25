c5efee69c9695ae7ededc36182c44ab2fbadf1e1
Rocco, Marco <7020500+ech0s7r@users.noreply.github.com>
2019-07-25 15:46:47 +0200

    use openapi-generator for Python client

---
+++ rest_client_python/openapi_client/api/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.delete_api import DeleteApi
+from openapi_client.api.get_asset_api import GetAssetApi
+from openapi_client.api.get_task_status_api import GetTaskStatusApi
+from openapi_client.api.get_token_api import GetTokenApi
+from openapi_client.api.search_tokens_api import SearchTokensApi
+from openapi_client.api.suspend_api import SuspendApi
+from openapi_client.api.tokenize_api import TokenizeApi
+from openapi_client.api.transact_api import TransactApi
+from openapi_client.api.unsuspend_api import UnsuspendApi
+from openapi_client.api.notify_token_updated_api import NotifyTokenUpdatedApi
