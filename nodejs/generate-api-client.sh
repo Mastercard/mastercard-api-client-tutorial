a9faa5c25b4f26ab9501d6f0512d4592edbe69fb
Rocco, Marco <7020500+ech0s7r@users.noreply.github.com>
2020-07-31 12:52:40 -0400

    rm generated tests from script

--- nodejs/generate-api-client.sh
+++ nodejs/generate-api-client.sh
@@ -1,1 +1,13 @@
 openapi-generator generate -i MDES_Digital_Enablement.yaml -g javascript -o . -c config.json
+
+rm -rf ./docs/
+rm git_push.sh
+rm .openapi-generator-ignore
+rm .travis.yml
+
+shopt -s extglob
+cd test
+rm -rf model
+cd api
+rm -fv !(TokenizeApi.spec.js)
+cd ../..
